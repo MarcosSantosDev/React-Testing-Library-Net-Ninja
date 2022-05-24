3bb8a729dc5163566d0fa647d2ece6b16bfa40fb
MarcosSantosDev <marcosadriano034@gmail.com>
2022-05-24 03:55:23 -0300

    test: hooks of the life cicle

--- src/components/FollowersList/__test__/FollowersList.spec.js
+++ src/components/FollowersList/__test__/FollowersList.spec.js
@@ -11,6 +11,21 @@ const MockFollowersList = () => {
 }
 
 describe('FollowersList', () => {
+  // hooks
+  beforeEach(() => {
+    console.log('RUNNING BEFORE EACH TESTS')
+  })
+  beforeAll(() => {
+    console.log('RUNNING ONCE BEFORE ALL TESTS')
+  })
+  afterEach(() => {
+    console.log('RUNNING AFTER EACH TESTS');
+  })
+  afterAll(() => {
+    console.log('RAN ONCE AFTER ALL TESTS')
+  })
+
+
   it('should render follower items', async () => {
     render(<MockFollowersList />);
 
